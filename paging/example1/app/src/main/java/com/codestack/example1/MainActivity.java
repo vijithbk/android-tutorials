e1c7e9d07e1ecb6fa71e6c394d10d3462ef68499
Vijith B <vijubalak@gmail.com>
2019-08-10 15:50:29 +0530

    PageListAdapter

--- paging/example1/app/src/main/java/com/codestack/example1/MainActivity.java
+++ paging/example1/app/src/main/java/com/codestack/example1/MainActivity.java
@@ -2,6 +2,11 @@ package com.codestack.example1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Call<StackApiResponse> call = RetrofitClient.getInstance()
+                .getApi()
+                .getAnswers(1, 50, "stackoverflow");
+
+        call.enqueue(new Callback<StackApiResponse>() {
+            @Override
+            public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
+                StackApiResponse stackApiResponse = response.body();
+
+                Toast.makeText(MainActivity.this, String.valueOf(stackApiResponse.items.size()), Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+            }
+        });
     }
 }
